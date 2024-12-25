5edb584a52f511a0b3f4ed9126a1f7247e486c44
yunhao2234 <24004510@siswa.um.edu.my>
2024-12-25 23:45:50 +0800

    Create ValidateInput.java

---
+++ src/ValidateInput.java
@@ -0,0 +1,23 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ValidateInput {
+    public boolean validateDate (String date){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date check = null;
+        dateFormat.setLenient(false);
+
+        try{
+            check = dateFormat.parse(date);
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean validatePriority (String priority){
+        return priority.equalsIgnoreCase("High") || priority.equalsIgnoreCase("Medium") || priority.equalsIgnoreCase("Low");
+    }
+}
