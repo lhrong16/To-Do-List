0a6319a25eccdd0971d2093cb1a66a72ce0467dd
LING HUA RONG <24004492@siswa.um.edu.my>
2024-12-27 00:00:11 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,139 +1,103 @@
-import java.util.Scanner;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalDate;
 
-public class Main {
-    public static void main(String[] args) {
-        TaskManager task = new TaskManager();
-        Mail mail = new Mail();
-        Scanner sc = new Scanner(System.in);
-        DataAnalytics analytics = new DataAnalytics(task);
-        dateCheckThread dateCheck = new dateCheckThread(task, mail);
+class dateCheckThread extends Thread {
+    private TaskManager task;
+    private Mail mail;
 
-        task.importSQL();
-        task.importDependencies();
+    public dateCheckThread(TaskManager task, Mail mail) {
+        this.task = task;
+        this.mail = mail;
+    }
 
-        boolean running = true;
-        while (running) {
-            System.out.print("""
-                             === Task Manager ===
-                             
-                             1. Add a New Task
-                             2. Edit Task
-                             3. Delete a Task
-                             4. Task Dependency
-                             5. Mark Task as Complete
-                             6. View All Tasks
-                             7. Search Tasks
-                             8. Show Analytics Dashboard
-                             9. Connect with Email
-                             10. Save and Close
-                             
-                             """);
-            System.out.print("Choose an option : ");
+    @Override
+    public void run() {
+        while (true) {
+            for (int i = 0; i < task.getTaskArrayList().size(); i++) {
+                Task check = task.getTaskArrayList().get(i);
 
-            int choice = sc.nextInt();
-            sc.nextLine();
-            System.out.println();
-
-            switch (choice) {
-                case 1 :
-                    System.out.print("""
-                                     === Add a New Task ===
-                                     
-                                     1. Not recurring task
-                                     2. Recurring task
-                                     
-                                     """);
-                    System.out.print("Choose an option : ");
-
-                    choice = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println();
-
-                    switch (choice) {
-                        case 1 :
-                            task.addTask();
-                            break;
-                        case 2:
-                            task.addRecurringTask();
-                            break;
-                    }
-
-                    break;
-                case 2 :
-                    task.editTask();
-                    break;
-                case 3 :
-                    task.deleteTask();
-                    break;
-                case 4 :
-                    System.out.print("""
-                                     === Task Dependency ===
-                                     
-                                     1. Set dependency
-                                     2. Delete dependency
-                                     
-                                     """);
-                    System.out.print("Choose an option : ");
-                    choice = sc.nextInt();
-                    System.out.println();
-                    sc.nextLine();
-
-                    switch (choice) {
-                        case 1 :
-                            task.setTaskDependency();
-                            break;
-                        case 2:
-                            task.deleteTaskDependency();
-                            break;
-                    }
-                    break;
-                case 5 :
-                    task.markTaskAsComplete();
-                    break;
-                case 6 :
-                    System.out.print("""
-                                     === View All Tasks ===
-                                     
-                                     1. Without sorting
-                                     2. Sorting
-                                     
-                                     """);
-                    System.out.print("Choose an option : ");
-
-                    choice = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println();
-
-                    switch (choice) {
-                        case 1 :
-                            task.viewTasks();
-                            break;
-                        case 2:
-                            task.sortTasks();
-                            task.viewTasks();
-                            break;
-                    }
-                    break;
-                case 7 :
-                    task.searchTasks();
-                    break;
-                case 8 :
-                    analytics.showAnalyticsDashboard();
-                    break;
-                case 9 :
-                    System.out.print("Enter Email Address : ");
-                    String email = sc.nextLine();
-                    mail.getEmail(email);
-                    dateCheck.start();
-                    break;
-                case 10 :
-                    task.closeTaskManager();
-                    System.exit(0);
-                default :
-                    System.out.println("Invalid choice. Please try again.");
+                if (mail.dateCheck(check.getTitle(), check.getDueDate(), check.getEmailFlag())) {
+                    check.setEmailFlag();
+                }
             }
         }
+    }
+}
+
+
+public class Mail {
+    private String recipient;
+
+    public void getEmail(String email){
+        recipient = email;
+    }
+
+    public boolean dateCheck(String t,String dd, boolean e) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date duedate, currentDate;
+
+        try {
+            duedate = dateFormat.parse(dd);
+            currentDate = dateFormat.parse(LocalDate.now().toString());
+            //System.out.println(duedate + " " + currentDate);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        if (duedate.compareTo(currentDate) < 1 && !e) {
+            sendMail(recipient,t);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void sendMail(String recipient,String title) {
+        Properties props = new Properties();
+
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        String myAccountEmail = "yunhao532@gmail.com";
+        String password = "hgri cfxn qeie oogo";
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+
+        Message message = prepareMessage(session, myAccountEmail, recipient, title);
+
+        try {
+            Transport.send(message);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        System.out.println("=== Email Notification===");
+        System.out.println("Sending reminder email for task \""+title+"\" due in 24 hours");
         System.out.println();
     }
 
+    private static Message prepareMessage (Session session, String myAccountEmail, String recipient, String title) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            message.setSubject("To-do-List task due in 24 hours");
+            message.setText("Reminder! Task \""+title+"\" due in 24 hours!");
+            return message;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
 }
